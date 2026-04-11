5a2e558895ff869eb4d48b1db4c4b80a8df86eec
Anthony Sheu <asheu@students.cs.ubc.ca>
2026-04-10 21:26:41 -0700

    Added basic navigation bar at the top to switch between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import ELSIPage from './pages/ELSI';
@@ -6,6 +6,14 @@ import ELSIPage from './pages/ELSI';
 function App() {
   return (
     <Router>
+        <nav className='w-full border-b px-6 py-4 flex gap-6 bg-white'>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+          <Link to="/elsi" className="hover:underline">
+            ELSI
+          </Link>
+        </nav>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/elsi" element={<ELSIPage />} />
