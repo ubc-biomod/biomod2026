287c826755fe913f6597c021f14d3ed837c6cf2a
piano <piano.trumpet.cello@gmail.com>
2026-08-01 01:33:00 -0700

    Sponsor layout finished. The Red, blue, and yellow boxes represent sponsor logos, which may be resized/reorganized depending on the number of sponsors per category.

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,10 +1,185 @@
 function SponsorsPage() {
   return (
-    <div style={{ backgroundColor: "#BCBDEF", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <h1 style={{ fontFamily: "'Inter', sans-serif", fontWeight: 700, fontSize: "3rem", color: "#000" }}>
-        Sponsors
-      </h1>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+
+
+
+
+
+      <div style={{margin: 20, flexDirection: "column", backgroundColor: "#BCBDEF", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center",  width: "90%"}}>
+      <div style={{margin: 80, width:"80%", height: "80%", backgroundColor: "rgba(255, 255, 255, 0.4)", display: "flex", flexDirection: "column", alignItems: "center"}}>
+
+        <h1 style={{fontFamily: "'Inter', sans-serif", fontWeight: 700, fontSize: "3rem", color: "#000000", marginTop: 80}}>
+
+        
+        BECOME A SPONSOR
+        </h1>
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center"}}>
+           <p style={{marginTop: 5, marginInline: 80}}>Want to support our cause? Become a sponsor! Our strong performances in previous BIOMOD competitions wouldn't be possible without sponsors like you.</p>
+           <div style={{marginBlock: 45, color:"#FFFFFF"}}>
+            <button style={{marginInline: 30, padding: 10, backgroundColor: "#664381"}}>
+
+              SPONSORSHIP PACKAGE
+           </button>
+
+           <button style={{marginInline: 30, padding: 10, backgroundColor: "#664381"}}>
+             BECOME A SPONSOR!
+           </button>
+           </div>
+        </div>
+      </div>
+      
+    
+      <p>[insert background image]</p>
+      
     </div>
+
+      <div style={{ height: "25%", width: "90%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 20,  fontFamily: "'Inter', sans-serif", fontWeight: 200}}>
+        <div style={{height: "25%", width: "90%", alignItems: "center", backgroundColor: "#CD7F32", display: "flex", justifyContent: "center"}}>
+          <p style={{}}>BRONZE TIER</p>
+          
+        </div>
+      </div>
+      <div style={{display: "flex", flexDirection: "row", alignItems: "flex-start"}}>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 190, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 200, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 170, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 200, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 180, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 160, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 150, height: 210, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 150, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 150, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 150, height: 190, margin: 10}}></div>
+        </div>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 125, height: 130, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 100, margin: 10}}></div>
+        </div>
+      </div>
+
+      <div style={{ height: "25%", width: "90%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 20,  fontFamily: "'Inter', sans-serif", fontWeight: 200}}>
+        <div style={{height: "25%", width: "90%", alignItems: "center", backgroundColor: "#D4AF37", display: "flex", justifyContent: "center"}}>
+          <p style={{}}>GOLD TIER</p>
+
+        </div>
+      </div>
+
+      <div style={{display: "flex", flexDirection: "row", alignItems: "flex-start"}}>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 190, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 200, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 170, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 200, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 180, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 160, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 150, height: 210, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 150, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 150, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 150, height: 190, margin: 10}}></div>
+        </div>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 125, height: 130, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 100, margin: 10}}></div>
+        </div>
+      </div>
+
+      <div style={{ height: "25%", width: "90%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 20,  fontFamily: "'Inter', sans-serif", fontWeight: 200}}>
+        <div style={{height: "25%", width: "90%", alignItems: "center", backgroundColor: "#808080", display: "flex", justifyContent: "center"}}>
+          <p style={{}}>PLATINUM TIER</p>
+
+        </div>
+      </div>
+
+      <div style={{display: "flex", flexDirection: "row", alignItems: "flex-start"}}>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 190, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 200, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 170, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 200, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 180, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 160, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 150, height: 210, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 150, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 150, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 150, height: 190, margin: 10}}></div>
+        </div>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 125, height: 130, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 100, margin: 10}}></div>
+        </div>
+      </div>
+
+      <div style={{ fontSize: "3rem", height: "25%", width: "90%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 20,  fontFamily: "'Inter', sans-serif", fontWeight: 700}}>
+        <h1>PAST SPONSORS</h1>
+      </div>
+
+      <div style={{display: "flex", flexDirection: "row", alignItems: "flex-start"}}>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 190, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 200, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 200, height: 170, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 200, height: 200, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 200, height: 180, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 200, height: 160, margin: 10}}></div>
+        </div>
+
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 150, height: 210, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 150, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 150, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 150, height: 190, margin: 10}}></div>
+        </div>
+        <div style={{display:"flex", flexDirection: "column", marginBlock: 10, alignItems: "flex-start"}}>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 160, margin: 10}}></div>
+          <div style={{backgroundColor: "yellow", width: 125, height: 130, margin: 10}}></div>
+          <div style={{backgroundColor: "red", width: 125, height: 150, margin: 10}}></div>
+          <div style={{backgroundColor: "blue", width: 125, height: 100, margin: 10}}></div>
+        </div>
+      </div>
+      
+
+    </div> 
   );
 }
 
